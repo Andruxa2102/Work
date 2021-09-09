398ad325710cf99fa35b2e3ea1345276c0775d05
Andruxa2102 <51362413+Andruxa2102@users.noreply.github.com>
2021-09-09 11:35:05 +0300

    Formatted for better look

--- conversions.sql
+++ conversions.sql
@@ -1,49 +1,52 @@
-
 -- 1. Еженедельная конверсия - приглашенные - оформленные (работающие).  В разрезе городов - проектов - вакансий 
-SELECT inv.project_id, inv.spec_id, inv.region_id, inv.cnt_invited, workd.cnt_working, (workd.cnt_working / inv.cnt_invited) AS convrersion
-FROM (SELECT project_id, spec_id, region_id, COUNT(*) AS cnt_invited
+SELECT inv.project_id, inv.spec_id, inv.region_id, inv.cnt_invited, workd.cnt_working, (workd.cnt_working / inv.cnt_invited) conversion
+FROM 
+	(SELECT project_id, spec_id, region_id, COUNT(*) cnt_invited
 FROM couriers 
 WHERE created BETWEEN '2021,9,1' AND '2021,9,10'
-GROUP BY project_id, spec_id) AS inv
-JOIN (SELECT project_id, spec_id, region_id, COUNT(*) AS cnt_working
+GROUP BY project_id, spec_id) inv
+JOIN 
+	(SELECT project_id, spec_id, region_id, COUNT(*) cnt_working
 	FROM couriers 
 	WHERE id IN 
-	(SELECT couriers.id FROM contracts 
+		(SELECT couriers.id FROM contracts 
 		JOIN couriers ON contracts.courier_id = couriers.id) 
 	AND created BETWEEN '2021,9,1' AND '2021,9,10'
-	GROUP BY project_id, spec_id, region_id) AS workd
+	GROUP BY project_id, spec_id, region_id) workd
 ON inv.project_id = workd.project_id AND inv.spec_id = workd.spec_id AND
 inv.region_id = workd.region_id;
 
 
 -- 2. Конверсия по этапам стажировки (отслеживать где у нас процент отсеивания самый большой идет)
-SELECT state_details, cnt, SUM(a.cnt) OVER() AS summa, cnt/(SUM(a.cnt) OVER()) AS conversion FROM 
-(SELECT state_details, COUNT(state_details) AS cnt FROM couriers
+SELECT state_details, cnt, SUM(a.cnt) OVER() summa, cnt/(SUM(a.cnt) OVER()) conversion
+FROM 
+	(SELECT state_details, COUNT(state_details) cnt FROM couriers
 WHERE state_details <> -1
-GROUP BY state_details) AS a
+GROUP BY state_details) a
 ORDER BY state_details;
 
+
 -- 3.1. Сколько обработал карточек (даже если с одной и той же работал, маркер - поставил коммент или поменял статус) во всех разделах.
-SELECT hhd.creator_id, u.surname, u.name, hhd.any_actions FROM
-(SELECT creator_id, COUNT(*) AS any_actions
-FROM history_details AS hd
+SELECT hhd.creator_id, u.surname, u.name, hhd.any_actions 
+FROM
+	(SELECT creator_id, COUNT(*) any_actions
+FROM history_details hd
 JOIN 
 (SELECT * FROM histories
-  WHERE object_type = 1 AND created >= '2021,9,1' AND created <= '2021,9,30' AND creator_id IN
-   (SELECT id FROM users WHERE department_id = 6)) AS h
-  ON h.id = hd.history_id
+WHERE object_type = 1 AND created >= '2021,9,1' AND created <= '2021,9,30' AND creator_id IN
+	(SELECT id FROM users WHERE department_id = 6)) h
+ON h.id = hd.history_id
 GROUP BY creator_id) AS hhd
-JOIN users AS u ON u.id = hhd.creator_id;
+JOIN users u ON u.id = hhd.creator_id;
 
 
 -- 3.2. Сколько пригласил (если раздел Контакты)/восстановил (если раздел исполнители, маркер - перевод в статус Новый или Восстановлен)
-SELECT h.creator_id, u.surname, u.name, COUNT(*) AS vosstan_priglasheno FROM history_details hd
+SELECT h.creator_id, u.surname, u.name, COUNT(*) vosstan_priglasheno FROM history_details hd
 JOIN histories h ON h.id = hd.history_id
 JOIN users u ON h.creator_id = u.id
-WHERE ((hd.field_name = 'StateId' AND hd.value_new = 37 AND h.object_type = 3) OR (hd.field_name = 'StateDetails' AND hd.value_new = 19 AND h.object_type = 1)) AND 
-	h.created >= '2021,9,1' AND h.created <= '2021,9,30' AND h.creator_id IN
-   (SELECT id FROM users WHERE department_id = 6)
-   GROUP BY h.creator_id
+WHERE ((hd.field_name = 'StateId' AND hd.value_new = 37 AND h.object_type = 3) OR (hd.field_name = 'StateDetails' AND hd.value_new = 19 AND h.object_type = 1)) AND h.created >= '2021,9,1' AND h.created <= '2021,9,30' AND h.creator_id IN
+	(SELECT id FROM users WHERE department_id = 6)
+	GROUP BY h.creator_id
 	ORDER BY vosstan_priglasheno;
 	
 	
